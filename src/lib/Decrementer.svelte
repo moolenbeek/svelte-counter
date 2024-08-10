43b184fd7e15b72598654352b7bb053c0c1496a7
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-10 14:01:49 -0400

    create counter

---
+++ src/lib/Decrementer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { count } from '../stores';
+
+	const decrement = () => {
+		count.update((n) => n - 1);
+	};
+</script>
+
+<button type="button" class="btn-icon variant-ghost" on:click={decrement}>-</button>
